09daa5e2946886fce6c9c746b2a74bd7e9e7b588
jundimuhammadalfatih <jundimuhammadalfatih@gmail.com>
2022-07-10 19:43:46 +0700

    update UI

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,8 +1,8 @@
 <div class="container-fluid">
     <div class="jumbotron">
         <h1 class="display-4">Hello, <?= $this->session->userdata('username') ?></h1>
-        <p class="lead">AR.EO Event Organizer</p>
+        <p class="lead">Selamat Datang di <b> AR.EO </b> ( AR Event Organizer )</p>
         <hr class="my-4">
-        <p>Terima kasih telah menggunakan layanan kami.</p>
+        <p>Terima Kasih telah menggunakan layanan kami. Kami akan terus berusaha memberikan pelayanan yang terbaik.</p>
     </div>
 </div>
--- application/views/templates/admin/footer.php
+++ application/views/templates/admin/footer.php
@@ -3,11 +3,8 @@
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
-    </div>
+  <strong>Develop By <a href="https://nurulfikri.ac.id/" target="_blank">AR.EO </a>|</strong> TI-17
+  <div class="float-right d-none d-sm-block"><b>STT-NF</b></div>
   </footer>
 
   <!-- Control Sidebar -->
