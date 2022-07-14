f9c276f4eae32cd831475fa0016d648e1736b97a
jundimuhammadalfatih <jundimuhammadalfatih@gmail.com>
2022-07-15 03:11:16 +0700

    update ui users

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -7,36 +7,38 @@
 				</div>
 				<!-- /.card-header -->
 				<div class="card-body">
-					<table id="example2" class="table table-bordered table-hover">
-						<thead>
-							<tr>
-								<th>No</th>
-								<th>Username</th>
-								<th>Email</th>
-								<th>Terakhir Login</th>
-								<th>Role</th>
-								<th>Status</th>
-								<th>Aksi</th>
-							</tr>
-						</thead>
-						<tbody>
-							<?php foreach ($users as $key => $value) : ?>
+					<div class="table-responsive">
+						<table id="example2" class="table table-bordered table-hover">
+							<thead>
 								<tr>
-									<td><?= ++$key ?></td>
-									<td><?= $value->username ?></td>
-									<td><?= $value->email ?></td>
-									<td><?= $value->last_login ?></td>
-									<td><?= $value->role ?></td>
-									<td><?= $value->status == 1 ? 'Aktif' : 'Tidak Aktif' ?></td>
-									<td>
-										<a href="<?= base_url('index.php/users/delete/'.$value->id) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')">
-											<i class="fas fa-trash"></i>
-										</a>
-									</td>
+									<th>No</th>
+									<th>Username</th>
+									<th>Email</th>
+									<th>Terakhir Login</th>
+									<th>Role</th>
+									<th>Status</th>
+									<th>Aksi</th>
 								</tr>
-							<?php endforeach ?>
-						</tbody>
-					</table>
+							</thead>
+							<tbody>
+								<?php foreach ($users as $key => $value) : ?>
+									<tr>
+										<td><?= ++$key ?></td>
+										<td><?= $value->username ?></td>
+										<td><?= $value->email ?></td>
+										<td><?= $value->last_login ?></td>
+										<td><?= $value->role ?></td>
+										<td><?= $value->status == 1 ? 'Aktif' : 'Tidak Aktif' ?></td>
+										<td>
+											<a href="<?= base_url('index.php/users/delete/'.$value->id) ?>" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')">
+												<i class="fas fa-trash"></i>
+											</a>
+										</td>
+									</tr>
+								<?php endforeach ?>
+							</tbody>
+						</table>
+					</div>
 				</div>
 				<!-- /.card-body -->
 			</div>
